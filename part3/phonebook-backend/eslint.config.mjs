111e6f0e5ab530ec9e2374b6ab86bb3a52ebe2fa
rezaHosseini <rezahosseiny1998@gmail.com>
2026-08-23 00:45:21 +0330

    deploy new build

---
+++ part3/phonebook-backend/eslint.config.mjs
@@ -0,0 +1,26 @@
+import js from "@eslint/js";
+import globals from "globals";
+
+export default [
+  js.configs.recommended,
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      sourceType: "commonjs",
+      globals: {
+        ...globals.node,
+      },
+      ecmaVersion: "latest",
+    },
+    rules: {
+      eqeqeq: "error",
+      "no-trailing-spaces": "error",
+      "object-curly-spacing": ["error", "always"],
+      "arrow-spacing": ["error", { before: true, after: true }],
+      "no-console": 0,
+    },
+  },
+  {
+    ignores: ["dist/**", "build/**", "node_modules/**"],
+  },
+];
